b17497fa03530f3d2ea81c27f37fac5acde67715
vbox <vbox@vbox-VirtualBox>
2019-02-24 22:48:57 +0100

    Add support for MIR3G router

--- release/src/router/rc/lan.c
+++ release/src/router/rc/lan.c
@@ -1184,7 +1184,15 @@ gen_ra_config(const char* wif)
 			else if (!strcmp(word, nvram_safe_get("wl1_ifname"))) // 5G
 			{
 				if (!strncmp(word, "rai", 3))	// iNIC
+#if defined(RTMIR3G)
+                                   {
+                                        gen_ralink_config(1, 1);
+                                        if  (!strcmp("1", nvram_safe_get("wl1_itxbf")))
+                                            system("sed s/ITxBfEn=0/ITxBfEn=1/ -i /etc/Wireless/iNIC/iNIC_ap.dat");
+                                   }
+#else
 					gen_ralink_config(1, 1);
+#endif
 				else
 					gen_ralink_config(1, 0);
 			}
--- release/src/router/shared/shared.h
+++ release/src/router/shared/shared.h
@@ -694,6 +694,7 @@ enum {
 	MODEL_GTAX6000,
 	MODEL_GTAX6000N,
 	MODEL_GTAX6000S,
+    MODEL_RTMIR3G,
 };
 
 /* NOTE: Do not insert new entries in the middle of this enum,
